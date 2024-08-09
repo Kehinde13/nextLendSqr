164c47fe1f977337bb6b3e3a5285aa4f3c4db81d
Kehinde Balogun <balogunkehinde3@gmail.com>
2024-08-09 18:08:06 -0400

    fixed toggle on desktop

--- app/ui/dashboard/Drawer.tsx
+++ app/ui/dashboard/Drawer.tsx
@@ -1,6 +1,4 @@
 import Sidebar from "./Sidebar";
-import menu from "@/public/icons/Menu.png";
-import Image from "next/image";
 import { Drawer, DrawerContent, DrawerTrigger } from "../shadcn/drawer";
 import { AlignJustifyIcon } from "lucide-react";
 
@@ -8,7 +6,7 @@ export default function DrawerContainer() {
   return (
     <Drawer>
       <DrawerTrigger>
-        <AlignJustifyIcon />
+        <AlignJustifyIcon className="md:hidden" />
       </DrawerTrigger>
       <DrawerContent>
         <Sidebar />
