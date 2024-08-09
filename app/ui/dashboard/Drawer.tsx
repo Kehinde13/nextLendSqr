e96e7469d55cdf54a58d5ec63ca737b1066175cc
Kehinde Balogun <balogunkehinde3@gmail.com>
2024-08-09 11:36:19 -0400

    fixed menu bar

--- app/ui/dashboard/Drawer.tsx
+++ app/ui/dashboard/Drawer.tsx
@@ -2,12 +2,13 @@ import Sidebar from "./Sidebar";
 import menu from "@/public/icons/Menu.png";
 import Image from "next/image";
 import { Drawer, DrawerContent, DrawerTrigger } from "../shadcn/drawer";
+import { AlignJustifyIcon } from "lucide-react";
 
 export default function DrawerContainer() {
   return (
     <Drawer>
       <DrawerTrigger>
-        <Image src={menu} alt="menu bar" className="md:hidden" />
+        <AlignJustifyIcon />
       </DrawerTrigger>
       <DrawerContent>
         <Sidebar />
